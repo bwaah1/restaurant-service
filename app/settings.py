27ce55d56cab23d7e03fc3157bd53d2144862c16
bwaah1 <chykivan@gmail.com>
2024-07-20 15:55:16 +0300

    Remove insecure DJANGO_SECRET_KEY for deployment

--- app/settings.py
+++ app/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = os.environ["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
